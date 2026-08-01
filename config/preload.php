1940e07ccd5d0e4b2262ac206bf82f99481b8c37
Maxime Valin <bysplashgm@gmail.com>
2026-08-01 21:56:07 +0200

    chore(config): add license header via CS Fixer header_comment (#60)

--- config/preload.php
+++ config/preload.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Pitlane.
+ *
+ * (c) Maxime Valin
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 if (file_exists(dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php')) {
     require dirname(__DIR__).'/var/cache/prod/App_KernelProdContainer.preload.php';
 }
